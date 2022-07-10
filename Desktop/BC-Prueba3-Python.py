5192e0cf0e09ccb1ef06d65466adb03f41a923a6
ronaldtro <ronaldcastro1512@gmail.com>
2022-07-10 16:43:34 -0500

    Subiendo Prueba3..

---
+++ Desktop/BC-Prueba3-Python.py
@@ -0,0 +1,19 @@
+nombre = "Ronald"
+apellidos = "Castro Torres"
+edad = 24
+email = "ronald@hotmail.com"
+telefono = 1234
+casado = False
+conHijos = False
+listaAmigos = ['Camila', 'Mariana', 'Julian', 'Juan']
+peliculasVistas = {'Pelicula1': 'Piratas del caribe', 'Pelicula2': 'Harry potter', 'Pelicula3': 'Soy leyenda'}
+
+print(nombre)
+print(apellidos)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(conHijos)
+print(listaAmigos)
+print(peliculasVistas)
