d45ffb81187965e9538076ecd19c54aed00388d4
ronaldtro <ronaldcastro1512@gmail.com>
2022-07-10 17:18:01 -0500

    Subiendo prueba4..

---
+++ Desktop/BC-Prueba4-Python.py
@@ -0,0 +1,8 @@
+
+peso = int(input('Digite su peso (KG): '))
+altura = float(input('Digite su altura (Metros): '))
+
+altura **= 2
+resultado = round(peso/altura, 2)
+
+print('Su indice de masa corporal es: ',resultado)
