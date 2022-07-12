550cf825f008bcf53fa4c32361165442028ecf51
ronaldtro <ronaldcastro1512@gmail.com>
2022-07-11 22:57:59 -0500

    Actualizando prueba 10..

--- Desktop/BC-Prueba10-Python.py
+++ Desktop/BC-Prueba10-Python.py
@@ -1,15 +1,22 @@
 
 class Vehiculo:
-    color = 'Azul'
-    ruedas = 4
-    puertas = 4
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+
 
 class Coche(Vehiculo):
-    velocidad = 5
-    cilindrada = 300
+    def __init__(self, color, rueda, puertas, velocidad, cilindrada):
+        super().__init__(color, rueda, puertas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return  "Color: {} Ruedas: {} Puertas: {} Velocidad: {} Cilindrada: {}".format(self.color, self.ruedas, self.puertas, self.velocidad, self.cilindrada)
 
 
-c1 = Coche()
+c1 = Coche('Rojo', 4, 4, 90, 700)
 print(c1)
 
 
