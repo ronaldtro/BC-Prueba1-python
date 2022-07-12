ca49a05efa4bbbb16f18a747090fcd20cf1c55e4
ronaldtro <ronaldcastro1512@gmail.com>
2022-07-11 23:35:12 -0500

    Actualizando prueba 10..

--- Desktop/BC-Prueba10-Python.py
+++ Desktop/BC-Prueba10-Python.py
@@ -20,4 +20,28 @@ c1 = Coche('Rojo', 4, 4, 90, 700)
 print(c1)
 
 
+class Alumno:
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
 
+    def notaFinal(self):
+        if self.nota >= 5 and self.nota <= 10:
+            print('Alumno: ',self.nombre)
+            print("Nota: ",self.nota)
+            print("Estado Aprobado")
+        elif self.nota < 5:
+            print('Alumno: ',self.nombre)
+            print("Nota: ",self.nota)
+            print("Estado No aprobado")  
+
+
+nombre = input('Digite su nombre: ')
+
+nota = -1
+while (nota < 0 or nota > 10):
+    nota = int(input('Digite la nota (0-10): '))
+
+
+a1 = Alumno(nombre, nota)         
+a1.notaFinal()
