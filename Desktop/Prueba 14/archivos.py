0d52fd2861a5067e8b30b805eb925bef1669d059
ronaldtro <ronaldcastro1512@gmail.com>
2022-07-12 22:01:31 -0500

    Subiendo prueba 14..

---
+++ Desktop/Prueba 14/archivos.py
@@ -0,0 +1,7 @@
+fichero = open('ejemplo.txt', 'w')
+fichero.close()
+
+fichero = open('ejemplo.txt', 'w')
+fichero.write('Escribiendo en el archivo..\n')
+fichero.write('Escribiendo en el archivo..\n')
+fichero.close()
